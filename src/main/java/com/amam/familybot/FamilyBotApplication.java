035cd3a5dc047da12e95b9be0d6c2d316b827651
AntonioBrightside <JilooPn/1013?>
2024-05-05 10:29:59 +0300

    Initial commit

---
+++ src/main/java/com/amam/familybot/FamilyBotApplication.java
@@ -0,0 +1,13 @@
+package com.amam.familybot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyBotApplication.class, args);
+	}
+
+}
