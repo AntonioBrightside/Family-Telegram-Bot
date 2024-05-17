0b158ec10d79ab3cb5777572e3754cc5db4b6aeb
AntonioBrightside <JilooPn/1013?>
2024-05-17 15:48:52 +0300

    Added: Sleep Time documentation

--- src/main/java/com/amam/familybot/service/SleepTimeService.java
+++ src/main/java/com/amam/familybot/service/SleepTimeService.java
@@ -30,7 +30,13 @@ public class SleepTimeService {
         this.sleepTimeRepository = sleepTimeRepository;
     }
 
-
+    /**
+     * Parse user's message due to regex and record it to DB
+     * @param userMessage user test message
+     * @param messageId ID message
+     * @param replyMessageId the ID of replied message
+     * @return bot status
+     */
     public String parseUserMessage(String userMessage, long messageId, long replyMessageId) {
 
         Matcher matcherTime = patternTime.matcher(userMessage);
@@ -60,6 +66,15 @@ public class SleepTimeService {
         throw new IncorrectFormatMessageException();
     }
 
+    /**
+     * Save the record to DB. Save in two stages: fall asleep time and wake-up time.
+     * @param messageId message ID
+     * @param date date of sleep
+     * @param fallASleepTime fall asleep time
+     * @param wakeUpTime wake-up time
+     * @param replyMessageId the ID of replied message
+     * @return
+     */
     @Transactional
     private String save(long messageId, @Nullable LocalDate date, @Nullable LocalTime fallASleepTime,
                         @Nullable LocalTime wakeUpTime, long replyMessageId) {
@@ -70,11 +85,8 @@ public class SleepTimeService {
             sleep = sleepTimeRepository.findByMessageId(replyMessageId)
                     .orElseThrow(SleepTimeNotFoundException::new);
 
-            long sleepTimeMinutes = MINUTES.between(sleep.getFallAsleepTime(), wakeUpTime);
-            LocalTime sleepTimeHm = LocalTime.MIN.plus(Duration.ofMinutes(sleepTimeMinutes));
-
             sleep.setWakeUpTime(wakeUpTime);
-            sleep.setSleepTime(sleepTimeHm);
+            sleep.setSleepTime(getSleepTimeInHmFormat(sleep.getFallAsleepTime(), wakeUpTime));
 
             sleepTimeRepository.save(sleep);
             return "В запись добавлено время пробуждения";
@@ -90,8 +102,24 @@ public class SleepTimeService {
         }
     }
 
+    /**
+     * Find message by message ID
+     * @param messageId
+     * @return record from DB or empty Optional
+     */
     private Optional<SleepTime> findMessageId(long messageId) {
         return sleepTimeRepository.findByMessageId(messageId);
+    }
+
+    /**
+     * Count sleep time and return it in necessary format
+     * @param fallAsleepTime fall asleep time in 'HH:mm' format
+     * @param wakeUpTime wake-up time in 'HH:mm' format
+     * @return sleep time in 'HH:mm' format
+     */
+    private LocalTime getSleepTimeInHmFormat(LocalTime fallAsleepTime, LocalTime wakeUpTime) {
+        long sleepTimeMinutes = MINUTES.between(fallAsleepTime, wakeUpTime);
+        return LocalTime.MIN.plus(Duration.ofMinutes(sleepTimeMinutes));
     }
 
 
